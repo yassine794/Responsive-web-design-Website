1cb26ca787b6b8f427c9c02141a3480f1d0d335e
yassine794 <dakhliayassine@gmail.com>
2026-06-04 12:45:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,45 @@
+/*!
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
 
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function (e) {
+    e.preventDefault();
+
+    const targetId = this.hash;
+    const target = $(targetId);
+
+    if (target.length) {
+        $('html, body').stop().animate(
+            {
+                scrollTop: target.offset().top - 72
+            },
+            50
+        );
+    }
+});
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+$(window).on('activate.bs.scrollspy', function () {
+    $('html, body').stop(); // prevents animation lock
+});
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
